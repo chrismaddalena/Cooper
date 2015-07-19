9abd856382a9e8ac57e26e8a3409fcc76ad05a39
Chris Maddalena <chris.maddalena@gmail.com>
2015-07-19 01:03:19 -0400

    Expanding functionality
    Created options for cooper.py. Still need to add code to make the
    script do something other than print “yay” when an option is used.

--- cooper.py
+++ cooper.py
@@ -12,6 +12,7 @@ import phishgate #Custom functions for generating phishgates!
 import phishemail #Custom functions for phishing emails!
 import phishexit #Custom functions for exit pages!
 import toolbox
+from optparse import OptionParser
 
 #Terminal colors!
 class bcolors:
@@ -24,18 +25,34 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-strUsage = "Usage: cooper.py URL_TO_CLONE SERVER_PORT (ex: cooper.py http://www.google.com 8000)"
+parser = OptionParser()
+parser.add_option("-p", "--phishgate",  action="store", type="string", dest="gate", help="Specifies URL to use to create phishgate")
+parser.add_option("-e", "--email",  action="store", type="string", dest="email", help="Specifies file to use to create phishing email template")
+parser.add_option("-x", "--exit",  action="store", type="string", dest="exit", help="Specifies URL to use to create an exit template")
+parser.add_option("-s", action="store", type="string", dest="server", help="Use to start HTTP server after template is created")
+(menu, args) = parser.parse_args()
 
-if len(sys.argv) < 3:
-    print strUsage
+if menu.gate or menu.email or menu.exit:
+	#TODO: Add code to run different functions based on options.
+	#TODO: Make sure only p,e, OR x can be used
+	#TODO: Make sure input for p, e, and x matches requirements
+	print "yay"
 else:
-	URL = sys.argv[1]
-	PORT = sys.argv[2]
+	parser.print_help()
+
+#OLD STUFFS
+#strUsage = "Usage: cooper.py URL_TO_CLONE SERVER_PORT (ex: cooper.py http://www.google.com 8000)"
+
+#if len(sys.argv) < 3:
+#    print strUsage
+#else:
+#	URL = sys.argv[1]
+#	PORT = sys.argv[2]
 	#Fetch the source of a given webpage and save it as index.html
-	toolbox.collectSource(URL)
+#	toolbox.collectSource(URL)
 	#Process the index.html to replace links with phishgate links
-	phishgate.replaceURL()
+#	phishgate.replaceURL()
 	#Process index.html to fix images that will break
-	phishgate.fixImageURL(URL)
+#	phishgate.fixImageURL(URL)
 	#Start the SimpleHTTP server to view final index.html file
 	#toolbox.startHTTPServer(PORT)
