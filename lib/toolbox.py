700f1cae68dabd0a9b16f632482c3e248263af4d
Chris Maddalena <chris.maddalena@gmail.com>
2015-07-25 01:32:31 -0400

    Added options for encoding and decoding
    Added -n option to encode a supplied image for embedding in a tempalte.
    Added a -d option to decode base64 and quoted-printable email source.

--- lib/toolbox.py
+++ lib/toolbox.py
@@ -4,29 +4,30 @@ import SimpleHTTPServer #For running the SimpleHTTP server
 import urllib #For wget-like action
 import requests #For wget-like action
 from BeautifulSoup import BeautifulSoup #For parsing HTML
-import codecs
+import codecs #Avoids encoding issue when opening HTML files, like apostrophes being replaced
+import base64 #For encoding images in base64
 
 #Terminal colors!
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
 
 #Takes a URL, scrape that webpage, and save source to source.html
 def collectSource(strURL):
-	print "[+] Grabbing source HTML from " + strURL
+	print bcolors.OKGREEN + "[+] " + bcolors.ENDC + "Grabbing source HTML from " + strURL
 	try:
 		page = requests.get(strURL)
 		source = page.text
 		sourceFile = codecs.open("source.html", "w", encoding='utf-8')
 		sourceFile.write(source)
 		sourceFile.close()
-		print bcolors.OKGREEN + "[+] Succesfully connected to " + strURL + bcolors.ENDC
+		print bcolors.OKGREEN + "[+] "  + bcolors.ENDC + "Succesfully connected to " + strURL
 	except:
 		#If scraping fails, all is lost and we can only exit
 		print bcolors.FAIL + "[-] Check URL - Must be valid (ex: http://www.foo.bar)" + bcolors.ENDC
@@ -35,7 +36,7 @@ def collectSource(strURL):
 #Takes a txt or html file, takes contents, and dumps it into a source.html file for modification
 #Original file is preserved and new source.html file is created with utf-8 encoding to avoid encoding issues later
 def openSource(strFile):
-	print "[+] Opening source HTML file: " + strFile
+	print bcolors.OKGREEN + "[+] " + bcolors.ENDC + "Opening source HTML file: " + strFile
 	try:
 		inputFile = open(strFile, "r")
 		source = inputFile.read()
@@ -52,8 +53,17 @@ def startHTTPServer(PORT):
 	handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 	try:
 		httpd = SocketServer.TCPServer(("", PORT), handler)
-		print "[+] Done. See output at 127.0.0.1:",PORT
-		print "[+] Use CTRL+C to kill the web server."
+		print bcolors.OKGREEN + "[+] "  + bcolors.ENDC + "Done. See output at 127.0.0.1:",PORT
+		print ncolors.WARNING + "[!] " + bcolors.ENDC + "Use CTRL+C to kill the web server."
 		httpd.serve_forever()
 	except:
 		print bcolors.FAIL + "[-] Server stopped or could not be started. Check port number." + bcolors.ENDC
+
+#Takes an image file, encodes it in Base64, and prints encoded output for embedding in a template
+def encodeImage(IMAGE):
+	#Encode in Base64 and print encoded string for copying
+	with open(IMAGE, "rb") as image:
+		print bcolors.OKGREEN + "[+] " + bcolors.ENDC + "Image has been encoded. Copy this string:\n"
+		img_64 = base64.b64encode(image.read())
+		print img_64 + "\n"
+		print ncolors.OKGREEN + "[+] " + bcolors.ENDC + "End of encoded string."
