173fc13cc9a1656c8267838cf6bc3e84e60cea27
Chris Maddalena <chris.maddalena@gmail.com>
2016-05-26 00:02:53 -0400

    More banners

--- lib/banners.py
+++ lib/banners.py
@@ -3,28 +3,47 @@ import random
 def printArt():
 	r1 = """\
 
-		  CCC
-		 C
-		 C    ooo ooo ppp  eee rrr
-		 C    o o o o p  p e e r
-		  CCC ooo ooo ppp  ee r
-					  p
-					  p          _
-								 |
-		o   o                  ^ |
-					  /^^^^^7  L_/
-		'  '     ,oO))))))))Oo,
-			   ,'))))))))))))))), /{
-		  '  ,'o  ))))))))))))))))={
-			 >    ))))))))))))))))={
-			 `,   ))))))\ \)))))))={
-			   ',))))))))\/)))))' \{
-				 '*O))))))))O*'
+  CCC
+ C
+ C    ooo ooo ppp  eee rrr
+ C    o o o o p  p e e r
+  CCC ooo ooo ppp  ee r
+			  p
+			  p          _
+						 |
+o   o                  ^ |
+			  /^^^^^7  L_/
+'  '     ,oO))))))))Oo,
+	   ,'))))))))))))))), /{
+  '  ,'o  ))))))))))))))))={
+	 >    ))))))))))))))))={
+	 `,   ))))))\ \)))))))={
+	   ',))))))))\/)))))' \{
+		 '*O))))))))O*'
 
 """
 
 	r2 = """
-COOPER!!
+Welcome to Cooper!
+
+		  CCC
+		 C
+		 C    ooo ooo ppp  eee rrr
+		 C    o o o o p  p e e r
+		  CCC ooo ooo ppp  ee  r
+					  p
+					  p
+					   ,-,
+					 ,' /
+				   ,'  (          _          _
+		   __...--'     `-....__,'(      _,-'/
+  _,---''''                     ````-._,'  ,'
+,'  o                                  `  <
+`.____  )))                          ...'  \\
+   `--..._        .   .__....----''''   `-. \\
+		  ```7--i-`.  \\                    `-`
+			 `.(    `-.`.
+			   `'      `'
 """
 
 	art = [r1,r2]
